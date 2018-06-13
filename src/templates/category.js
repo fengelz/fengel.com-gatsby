fca8df22987b393eb6b381539baaf1c638837332
Sune <fengel@gmail.com>
2018-06-13 14:04:42 +0200

    Added date

--- src/templates/category.js
+++ src/templates/category.js
@@ -22,6 +22,7 @@ export const pageQuery = graphql`
           title
           excerpt
           slug
+          date
         }
       }
     }
--- src/templates/tag.js
+++ src/templates/tag.js
@@ -24,6 +24,7 @@ export const pageQuery = graphql`
           title
           excerpt
           slug
+          date
         }
       }
     }
